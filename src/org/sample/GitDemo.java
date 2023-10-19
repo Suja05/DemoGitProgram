d92b9cb9aaa0fcb48889946cd4663f2c3186613a
Suja05 <estherj030599@gmail.com>
2023-10-19 15:10:42 +0530

    first git program

---
+++ src/org/sample/GitDemo.java
@@ -0,0 +1,11 @@
+package org.sample;
+
+public class GitDemo {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hi Suja");
+		 System.out.println("Welcome to Git Project");
+	}
+
+}
